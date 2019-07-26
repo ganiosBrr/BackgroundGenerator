f3e7a346d0e37a4414eb4d643f61fd35a6c626a3
Ganibek <nurpeisovganibek@gmail.com>
2019-07-26 13:19:07 +0600

    Background color generator

---
+++ script.js
@@ -0,0 +1,16 @@
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var cssHeader = document.querySelector("css"); 
+var body = document.getElementById("gradient");
+
+
+function gradient() {
+    body.style.background = "linear-gradient(to right," 
+    + color1.value 
+    + "," 
+    + color2.value 
+    + ")"; 
+}
+
+color1.addEventListener("input", gradient);
+color2.addEventListener("input", gradient);
